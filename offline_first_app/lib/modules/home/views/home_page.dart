fb9f1b88ac6c6e91e9aa4a5ea545c3a880e860b4
Arthur Giani <arthur_giani@hotmail.com>
2021-04-17 14:46:38 -0300

    offline-first -  null safety migration started

--- offline_first_app/lib/modules/home/views/home_page.dart
+++ offline_first_app/lib/modules/home/views/home_page.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:mobx/mobx.dart';
 import 'home_controller.dart';
 import 'home_repository.dart';
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  HomeController homeController;
+  late final HomeController homeController;
 
   @override
   void initState() {
@@ -23,38 +21,26 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Offline First App Example'),
-      ),
-      body: Observer(builder: (_) {
-        switch (homeController.pokemonFuture.status) {
-          case FutureStatus.pending:
-            return Center(child: CircularProgressIndicator());
-            break;
-          case FutureStatus.fulfilled:
-            return SingleChildScrollView(
-              physics: ScrollPhysics(),
-              child: Column(
-                children: [
-                  ListView.builder(
-                    physics: ScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: homeController.pokemonList.length,
-                    itemBuilder: (context, index) {
-                      final pokemon = homeController.pokemonList[index];
-                      return ListTile(
-                        title: Text(pokemon.name),
-                      );
-                    },
-                  )
-                ],
-              ),
-            );
-          default:
-            return Text("Erro");
-        }
-      }),
-      //
-    );
+        appBar: AppBar(
+          title: Text('Offline First App Example'),
+        ),
+        body: SingleChildScrollView(
+          physics: ScrollPhysics(),
+          child: Column(
+            children: [
+              ListView.builder(
+                physics: ScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: homeController.pokemonList.length,
+                itemBuilder: (context, index) {
+                  final pokemon = homeController.pokemonList[index];
+                  return ListTile(
+                    title: Text(pokemon.name),
+                  );
+                },
+              )
+            ],
+          ),
+        ));
   }
 }
